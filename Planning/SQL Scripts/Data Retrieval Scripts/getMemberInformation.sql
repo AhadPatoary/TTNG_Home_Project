22fb7d651f71d6ca6d75c384de4844d6ac8b199f
AhadPatoary <ahad_p@outlook.com>
2024-08-05 00:06:59 +0100

    Connecting DB and Setting up Static Files

--- Planning/SQL Scripts/Data Retrieval Scripts/getMemberInformation.sql
+++ Planning/SQL Scripts/Data Retrieval Scripts/getMemberInformation.sql
@@ -1,3 +1,4 @@
 SELECT accountref, companyname, member.firstname , member.lastname, member.email
 FROM companylist
-INNER JOIN member ON companylist.memberID = member.memberID;
+INNER JOIN member ON companylist.memberID = member.memberID
+WHERE accountref LIKE '1001';
--- ttng/ttng/settings.py
+++ ttng/ttng/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adds',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ttng.urls'
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'ttng.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
     }
 }
 
@@ -115,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
